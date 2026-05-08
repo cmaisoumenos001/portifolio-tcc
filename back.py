d647b38a78d8d19581cc63d95a92f069e4082909
Murilo <mufe.cremonesi@gmail.com>
2026-05-08 11:00:23 -0300

    uptade html

--- back.py
+++ back.py
@@ -1,10 +1,22 @@
-from flask import Flask 
+from flask import Flask, render_template, request 
 
 app = Flask("back.py")
 
 @app.route("/")
 def home():
-    return "Olá, Flask!"
+    return render_template("index.html")
+
+@app.route("/cadastro")
+def cadastro():
+    return render_template("cadastro.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/ligas")
+def ligas():
+    return render_template("ligas.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
